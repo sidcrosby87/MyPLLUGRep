8ef8fd5b04583e0aede5cf93a73c7e37d9d4b6b2
sidcrosby87 <ikhaustov7c7@gmail.com>
2017-11-11 17:52:16 +0200

    nn

---
+++ ApertureTest/Cylinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApertureTest
+{
+    class Cylinder
+    {
+        double height, diameter;
+
+        public Cylinder(double height, double diameter)
+        {
+            this.height = height;
+            this.diameter = diameter;
+        }
+
+        internal double Height { get { return height; } }
+
+        internal double Diameter { get { return diameter; } }
+    }
+
+}
+
